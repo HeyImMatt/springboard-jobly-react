0061c93445adb9813dcbf60b9a68214e877b7ffe
heyimmatt <mrmcfarland@gmail.com>
2020-10-27 09:02:26 -0700

    Add home component

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
-    <div>
-    </div>
+    <Container className="mt-5">
+      <Row className="justify-content-center mt-5 pt-3">
+        <Col className="text-center" md={8}>
+          <h2 className="mt-5 display-3">Jobly</h2>
+          <h5>All the jobs in one, convenient place.</h5>
+          <Link to={"/login"} className="btn btn-primary mt-3">Login or Register</Link>
+        </Col>
+      </Row>
+    </Container>
   )
 }
