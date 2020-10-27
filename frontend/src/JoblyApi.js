c6a7049e7ec6e1c9a6bf0e878149ff207e728aaa
heyimmatt <mrmcfarland@gmail.com>
2020-10-27 13:11:12 -0700

    ready to deploy frontend

--- frontend/src/JoblyApi.js
+++ frontend/src/JoblyApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { TOKEN_STORAGE_ID } from "./App.js"
 
-const BASE_URL = process.env.BASE_URL || "http://localhost:3001";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
 export default class JoblyApi {
   static async request(endpoint, params = {}, verb = "get") {
